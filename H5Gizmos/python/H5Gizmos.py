4384f6795671db45cde6d95bbcbdaffce1a04318
Aaron Watters <awatters@simonsfoundation.org>
2022-01-03 12:56:26 -0500

    start link option

--- H5Gizmos/python/H5Gizmos.py
+++ H5Gizmos/python/H5Gizmos.py
@@ -233,6 +233,17 @@ class Gizmo:
     def _confirm_start(self):
         self._start_confirm_future.set_result(True)
 
+    def _show_start_link(self):
+        from IPython.display import HTML, display
+        url = self._entry_url()
+        link = '<a href="%s">gizmo link.</a> \n (%s)' % (url, url)
+        if gizmo_server.isnotebook():
+            msg = "<h4>Open gizmo using link</h4>\n" + link
+            display(HTML(msg))
+        else:
+            msg = "Open gizmo using link (control-click / open link)\n\n" + link + "\n\n"
+            print (msg)
+
     def _open_in_browser(self, new_page=True):
         import webbrowser
         url = self._entry_url()
