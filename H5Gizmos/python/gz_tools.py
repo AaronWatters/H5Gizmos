ba3f86d083999f86629df159d1365a0d07ae5f98
Aaron Watters <awatters@simonsfoundation.org>
2022-01-19 10:27:03 -0500

    generalization for multiple frames

--- H5Gizmos/python/gz_tools.py
+++ H5Gizmos/python/gz_tools.py
@@ -5,7 +5,7 @@ Miscellaneous helpers for interacting with H5Gizmos.
 from .. import hex_to_bytearray
 import numpy as np
 
-def get_snapshot_array(pixel_info, invert=False):
+def get_snapshot_arrays(pixel_info, invert=False):
     """
     Convert pixel data from an HTML canvas to an appropriate numpy array.
     Data coming from a 2d canvas is right side up -- data coming from WebGL must be inverted.
@@ -20,10 +20,16 @@ def get_snapshot_array(pixel_info, invert=False):
         data_bytes = bytearray(data_bytes)
     width = pixel_info["width"]
     height = pixel_info["height"]
-    bytes_per_pixel = 4
+    count = pixel_info.get("count", 1)
+    bytes_per_pixel = pixel_info.get("bands", 4)
     array1d = np.array(data_bytes, dtype=np.ubyte)
-    image_array = array1d.reshape((height, width, bytes_per_pixel))
+    image_array = array1d.reshape((count, height, width, bytes_per_pixel))
     if invert:
         # invert the rows.
-        image_array = image_array[::-1]
+        image_array = image_array[:, ::-1]
     return image_array
+
+def get_snapshot_array(pixel_info, invert=False):
+    arrays = get_snapshot_arrays(pixel_info, invert)
+    assert len(arrays) == 1, "too many arrays: " + repr(len(arrays))
+    return arrays[0]
