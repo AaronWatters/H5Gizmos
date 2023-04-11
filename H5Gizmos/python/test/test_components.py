599ff03d906f20d91237cd751a24aa8feefce0ce
Aaron Watters <awatters@flatironinstitute.org>
2023-04-11 14:54:43 -0400

    general refactor

--- H5Gizmos/python/test/test_components.py
+++ H5Gizmos/python/test/test_components.py
@@ -12,39 +12,50 @@ from multiprocessing import Process
 import H5Gizmos as gz
 import sys
 import unittest
+import asyncio
 
 
-def selenium_watcher_process(url, identity, snooze, retries):
-    #print("watcher: getting driver")
+def selenium_watcher_process(url, identity, snooze, retries, verbose=False):
+    if verbose:
+        print("watcher: getting driver")
     driver = webdriver.Chrome()
-    #print("watcher: getting url", repr(url))
+    if verbose:
+        print("watcher: sleeping before url", url)
+        time.sleep(60)
+        print("watcher: getting url", repr(url))
     driver.get(url)
     #assert "Python" in driver.title
     #identity = "xxxx"
     elem = None
-    #print("searching for identity", identity)
+    if verbose:
+        print("searching for identity", identity)
     for i in range(retries):
         time.sleep (snooze)
         try:
             elem = driver.find_element(By.ID, identity)
         except NoSuchElementException: 
-            #print ("watcher", identity, "not found")
+            if verbose:
+                print ("watcher", identity, "not found")
             pass
     if elem is not None:
-        #print("watcher: element found!  sleeping")
-        #time.sleep(snooze)
+        if verbose:
+            print("watcher: element found!")
+            #time.sleep(snooze * 10)
         pass
     else:
-        #print("watcher: giving up...")
+        if verbose:
+            print("watcher: giving up...")
         pass
     driver.close()
-    #print("watcher: close...")
+    if verbose:
+        print("watcher: close... sleeping")
+        time.sleep(snooze * 10)
 
 class ComponentCase(unittest.IsolatedAsyncioTestCase):
 
     snooze = 1
     retries = 10
-    html = "<h1>Initial test component</h1>"
+    #html = "<h1>Initial test component</h1>"
 
     def fullname(o):
         # https://stackoverflow.com/questions/2020014/get-fully-qualified-class-name-of-an-object-in-python
@@ -66,6 +77,7 @@ class ComponentCase(unittest.IsolatedAsyncioTestCase):
         await self.main_component.link(
             await_start=False,
             shutdown_on_close=False,
+            title = repr(fn) + " test"
             )
         url = self.main_component.gizmo._entry_url()
         args = (url, self.endid, self.snooze, self.retries)
@@ -82,19 +94,38 @@ class ComponentCase(unittest.IsolatedAsyncioTestCase):
         identity = self.endid
         sentinel = gz.Html('<div id="%s">%s</div>' % (identity, identity))
         self.main_component.add(sentinel)
+        # sleep to allow add to transmit
+        await asyncio.sleep(self.snooze)
         print( "gizmo: waiting for watcher to exit")
         self.watcher_process.join(self.snooze * self.retries)
-        print( "gizmo: watcher exitted")
+        print( "gizmo: watcher, sleeping to free up connections...")
+        time.sleep(self.snooze)
         #sys.exit()
 
-class HeaderTest(ComponentCase):
+class BasicTest(ComponentCase):
     txt = "Initial header test component"
     html = "<h1>%s</h1>" % txt
 
-    async def test_header(self):
+    async def logic(self):
+        txt = await gz.get(self.main_component.element.html())
+        self.assertEqual(txt, self.txt)
+
+    async def test_main(self):
         await self.startup()
         try:
-            txt = await gz.get(self.main_component.element.html())
-            self.assertEqual(txt, self.txt)
+            await self.logic()
         finally:
             await self.shutdown()
+
+class CompositeTest(BasicTest):
+
+    def make_main_component(self):
+        children = [
+            "Example Stack",
+            "End of stack"
+        ]
+        S = gz.Stack(children, css={"background-color": "cornsilk"})
+        return S
+    
+    async def logic(self):
+        pass
