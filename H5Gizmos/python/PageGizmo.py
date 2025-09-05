8b66cffc4090bf765cdee2a493943df47b6f0d87
Aaron Watters <awatters@flatironinstitute.org>
2025-09-05 14:45:10 -0400

    locate element on existing page

--- H5Gizmos/python/PageGizmo.py
+++ H5Gizmos/python/PageGizmo.py
@@ -16,10 +16,10 @@ class PageGizmo(gz_jQuery.jQueryComponent):
         assert os.path.isdir(folder), "no such folder: " + repr(folder)
         # read and customize the html source
         html = gz_resources.custom_html_file(entry_page_path)
-        print("html")
-        print(html)
-        print("end html")
-        super().__init__()
+        #print("html")
+        #print(html)
+        #print("end html")
+        super().__init__(init_text="")
         self.template = html
         for filename in os.listdir(folder):
             filepath = os.path.join(folder, filename)
@@ -27,3 +27,37 @@ class PageGizmo(gz_jQuery.jQueryComponent):
                 self.serve_folder(filename, filepath)
             else:
                 self.add_content(filepath)
+
+    def locate(self, selector):
+        """
+        Locate an element in the page by jQuery selector.
+        Return a jQueryComponent for that element.
+        """
+        result = SelectedElement(selector)
+        result.gizmo = self.gizmo
+        result.dom_element_reference(self.gizmo)
+        return result
+    
+    def locate_id(self, id):
+        """
+        Locate an element in the page by id.
+        Return a jQueryComponent for that element.
+        """
+        return self.locate("#" + id)
+    
+
+class SelectedElement(gz_jQuery.jQueryComponent):
+    """
+    A jQueryComponent that is a selection of elements from an existing page.
+    """
+
+    def __init__(self, selector):
+        self.selector = selector
+        self.location = None # set when attached to a gizmo
+        super().__init__(init_text="", tag=selector)
+
+    def dom_element_reference(self, gizmo):
+        #super().dom_element_reference(gizmo)
+        self.element = gizmo.jQuery(self.selector)
+        #print("element", self.element)
+        return self.element[0]
